83a132766f901418d085f660325e1b8f7ea68e76
chenqianhe <1278095698@qq.com>
2023-07-04 23:25:49 +0800

    fix: add analytics

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
+import { inject } from '@vercel/analytics';
 
 import "./assets/main.css";
 import { useCharacterStore } from "@/stores/characters";
+
+inject();
 
 const app = createApp(App);
 
